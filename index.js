a2dcfd28d653347f87dfd549fedbdba8fb903d0d
Emily <emilyhigginson14@gmail.com>
2022-01-28 14:35:44 -0500

    commit tree f678c4159984a17bb2576e4adebcecf74852011b

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+
+let createEmployeeRecord = function(row){
+    return {
+        firstName: row[0],
+        familyName: row[1],
+        title: row[2],
+        payPerHour: row[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+let createEmployeeRecords = function() {
+    return {
+
+    }
+}
+let createTimeOut = function() {
+    
+}
